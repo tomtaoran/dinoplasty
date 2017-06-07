d94657d17d1ed8e428ed01ca867e1ff5559c1add
Tom <tomtaoran@aliyun.com>
2017-06-07 16:15:58 -0400

    Create a dino object with a name and id

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 //console.log('It worked!')
 const app ={
     init(formSelector){
+        this.max=0
         document
             .querySelector(formSelector)
-            .addEventListener('submit',this.addDino)
+            .addEventListener('submit',this.addDino.bind(this))
     },
 
     addDino(ev){
         ev.preventDefault()
-        const dinoName=ev.target.dinoName.value
-        console.log(dinoName)
+        const dino ={
+            id: this.max + 1,
+            name: ev.target.dinoName.value
+        }
+        console.log(dino.name, dino.id)
+        ++ this.max
     },
 }
 app.init('#dino-form')
