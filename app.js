eaa6323d901557431ddb9851fc9a1d4e86115c0e
Tom <tomtaoran@aliyun.com>
2017-06-12 04:53:48 -0400

    Beautify the website

--- app.js
+++ app.js
@@ -65,10 +65,6 @@ const app ={
         ++ this.max
     },
 
-    handlePromote(ev){
-    ev.preventDefault()
-    const b= ev.target
-    },
 
     renderListItem(dino){
         
@@ -78,29 +74,39 @@ const app ={
         
         item.classList.remove('template') //Make them appear again, after taking the template
         if(dino.promoted===1){
-        item.style.color="red"
+        item.style.backgroundColor="rgb(235, 89, 255)"
         }
         item.querySelector('.dino-type').textContent = dino.type
         item.querySelector('.between-space').textContent = " : "
         item.querySelector('.dino-name').textContent = dino.name
-        
+        const hiddenButton = document.createElement("button");
+        hiddenButton.innerHTML = "hidden";
+        hiddenButton.classList.add("button-group")
+        item.appendChild(hiddenButton)
         item.querySelector('.dino-type').addEventListener('click', function(){
          if(item.classList.contains("clicked")){
              return // We don't do anything if it has been previously clicked
          }else{
-            item.classList.add("clicked");
+           item.classList.add("clicked");
+            const listItemSpan = document.createElement('span');
+            listItemSpan.classList.add("button-group");
+            listItemSpan.style.float="right"
+            listItemSpan.style.verticalAlign="middle"
             const saveButton = document.createElement("button");
             saveButton.setAttribute("class",dino.id)
             saveButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+            
             saveButton.innerHTML = "Save my change";
             saveButton.style.marginLeft= "1rem"
-            saveButton.style.marginRight= "1px"
+            saveButton.style.marginRight= "1rem"
+            saveButton.style.lineHeight="0"
             saveButton.addEventListener('click',function(){
                dino.type=item.querySelector('.dino-type').textContent
                dino.name=item.querySelector('.dino-name').textContent
                this.save()
             }.bind(this))
-            item.appendChild(saveButton)
+            listItemSpan.appendChild(saveButton)
+            item.appendChild(listItemSpan)
          }
          
          //alert(this.clicked)
@@ -110,109 +116,128 @@ const app ={
              return // We don't do anything if it has been previously clicked
          }else{
             item.classList.add("clicked");
+            const listItemSpan = document.createElement('span');
+            listItemSpan.classList.add("button-group");
+            listItemSpan.style.float="right"
+            listItemSpan.style.verticalAlign="middle"
             const saveButton = document.createElement("button");
             saveButton.setAttribute("class",dino.id)
             saveButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+            
             saveButton.innerHTML = "Save my change";
             saveButton.style.marginLeft= "1rem"
-            saveButton.style.marginRight= "1px"
+            saveButton.style.marginRight= "1rem"
+            saveButton.style.lineHeight="0"
             saveButton.addEventListener('click',function(){
                dino.type=item.querySelector('.dino-type').textContent
                dino.name=item.querySelector('.dino-name').textContent
                this.save()
             }.bind(this))
-            item.appendChild(saveButton)
+            listItemSpan.appendChild(saveButton)
+            item.appendChild(listItemSpan)
          }
         }.bind(this))
         
 
         
-
-        
-        item.querySelector('.dino-name').textContent +=' '+dino.id //For Debugging on ID issue
+        const index= this.dinos.indexOf(dino)
+        //item.querySelector('.dino-name').textContent +=' '+dino.id //For Debugging on ID issue
         item.querySelector('button.remove').addEventListener('click',this.removeDino.bind(this))
+        item.querySelector('button.fav').addEventListener('click',this.favDino.bind(this))
+        item.querySelector('button.up').addEventListener('click',this.moveUP.bind(this))
+        item.querySelector('button.down').addEventListener('click',this.moveDown.bind(this))
+        if(index===0){
+            item.querySelector('button.up').disabled="true"
+        }
+        if(index===this.dinos.length-1){
+            item.querySelector('button.down').disabled="true"
+        }
+
+
         item.dataset.id=dino.id  //A Neat Way to store data at backend
-        const promoteButton = document.createElement("button");
-        promoteButton.setAttribute("class",dino.id)
-        promoteButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
-        promoteButton.innerHTML = "promote";
-        promoteButton.style.marginLeft= "1rem"
-        promoteButton.style.marginRight= "1px"
-        promoteButton.addEventListener('click',function(){
-         dino.promoted=1
-         //localStorage.setItem('dinoplasty', JSON.stringify(this.dinos));
-         this.save()
-        }.bind(this))
-        //This is redundant, but it seems to be the only way to render on-the-spot
-        const deleteButton = document.createElement("button");
-        deleteButton.setAttribute("class",dino.id)
-        deleteButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
-        deleteButton.style.marginLeft= "1px"
-        deleteButton.style.marginRight= "1px"
-        deleteButton.innerHTML = "delete";
-        deleteButton.addEventListener('click',function(){
-         
-         const index= this.dinos.indexOf(dino)
-         this.dinos.splice(index, 1);
-         //alert(this.dinos)
-         item.remove()
-         //localStorage.setItem('dinoplasty', JSON.stringify(this.dinos));
-         this.save()
-        }.bind(this))
 
-        const moveUpButton = document.createElement("button");
-        moveUpButton.setAttribute("class",dino.id)
-       moveUpButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
-        moveUpButton.style.marginLeft= "1px"
-        moveUpButton.style.marginRight= "1px"
-        moveUpButton.innerHTML = "Up";
-       moveUpButton.addEventListener('click',function(){
-         const index= this.dinos.indexOf(dino)
-         if(index>0){
-             const dinoBefore=this.dinos[index-1];
-             const dinoNow=this.dinos[index];
-             this.dinos[index]=dinoBefore
-             this.dinos[index-1]=dinoNow
-            //Debugging: alert(this.dinos)
-            while(this.list.firstChild){
-            this.list.removeChild(this.list.firstChild);
-            }
-            for (let index = 0; index < this.dinos.length; index++) {
-            this.list.appendChild(this.renderListItem(this.dinos[index]))
-            }
-        }
-        }.bind(this))
-
-        const moveDownButton = document.createElement("button");
-        moveDownButton.setAttribute("class",dino.id)
-       moveDownButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
-        moveDownButton.style.marginLeft= "1px"
-        moveDownButton.style.marginRight= "1px"
-        moveDownButton.innerHTML = "Down";
-       moveDownButton.addEventListener('click',function(){
-         const index= this.dinos.indexOf(dino)
-         if(index<this.dinos.length-1){
-             const dinoNext=this.dinos[index+1];
-             const dinoNow=this.dinos[index];
-             this.dinos[index]=dinoNext
-             this.dinos[index+1]=dinoNow
-            //Debugging: alert(this.dinos)
-            while(this.list.firstChild){
-            this.list.removeChild(this.list.firstChild);
-            }
-            for (let index = 0; index < this.dinos.length; index++) {
-            this.list.appendChild(this.renderListItem(this.dinos[index]))
-            }
-        }
-        }.bind(this))
     
-        promoteButton.addEventListener('click',function(){
-         item.style.color="red"
-        })
-        item.appendChild(promoteButton)
-        item.appendChild(deleteButton)
-        item.appendChild(moveUpButton)
-        item.appendChild(moveDownButton)
+    //     const promoteButton = document.createElement("button");
+    //     promoteButton.setAttribute("class",dino.id)
+    //     promoteButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+    //     promoteButton.innerHTML = "promote";
+    //     promoteButton.style.marginLeft= "1rem"
+    //     promoteButton.style.marginRight= "1px"
+    //     promoteButton.addEventListener('click',function(){
+    //      dino.promoted=1
+    //      //localStorage.setItem('dinoplasty', JSON.stringify(this.dinos));
+    //      this.save()
+    //     }.bind(this))
+    //     //This is redundant, but it seems to be the only way to render on-the-spot
+    //     const deleteButton = document.createElement("button");
+    //     deleteButton.setAttribute("class",dino.id)
+    //     deleteButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+    //     deleteButton.style.marginLeft= "1px"
+    //     deleteButton.style.marginRight= "1px"
+    //     deleteButton.innerHTML = "delete";
+    //     deleteButton.addEventListener('click',function(){
+         
+    //      const index= this.dinos.indexOf(dino)
+    //      this.dinos.splice(index, 1);
+    //      //alert(this.dinos)
+    //      item.remove()
+    //      //localStorage.setItem('dinoplasty', JSON.stringify(this.dinos));
+    //      this.save()
+    //     }.bind(this))
+
+    //     const moveUpButton = document.createElement("button");
+    //     moveUpButton.setAttribute("class",dino.id)
+    //    moveUpButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+    //     moveUpButton.style.marginLeft= "1px"
+    //     moveUpButton.style.marginRight= "1px"
+    //     moveUpButton.innerHTML = "Up";
+    //    moveUpButton.addEventListener('click',function(){
+    //      const index= this.dinos.indexOf(dino)
+    //      if(index>0){
+    //          const dinoBefore=this.dinos[index-1];
+    //          const dinoNow=this.dinos[index];
+    //          this.dinos[index]=dinoBefore
+    //          this.dinos[index-1]=dinoNow
+    //         //Debugging: alert(this.dinos)
+    //         while(this.list.firstChild){
+    //         this.list.removeChild(this.list.firstChild);
+    //         }
+    //         for (let index = 0; index < this.dinos.length; index++) {
+    //         this.list.appendChild(this.renderListItem(this.dinos[index]))
+    //         }
+    //     }
+    //     }.bind(this))
+
+    //     const moveDownButton = document.createElement("button");
+    //     moveDownButton.setAttribute("class",dino.id)
+    //    moveDownButton.className += " button success"; //Let's set it button success for now, we can make it better, won't we?
+    //     moveDownButton.style.marginLeft= "1px"
+    //     moveDownButton.style.marginRight= "1px"
+    //     moveDownButton.innerHTML = "Down";
+    //    moveDownButton.addEventListener('click',function(){
+    //      const index= this.dinos.indexOf(dino)
+    //      if(index<this.dinos.length-1){
+    //          const dinoNext=this.dinos[index+1];
+    //          const dinoNow=this.dinos[index];
+    //          this.dinos[index]=dinoNext
+    //          this.dinos[index+1]=dinoNow
+    //         //Debugging: alert(this.dinos)
+    //         while(this.list.firstChild){
+    //         this.list.removeChild(this.list.firstChild);
+    //         }
+    //         for (let index = 0; index < this.dinos.length; index++) {
+    //         this.list.appendChild(this.renderListItem(this.dinos[index]))
+    //         }
+    //     }
+    //     }.bind(this))
+    
+    //     promoteButton.addEventListener('click',function(){
+    //      item.style.backgroundColor="rgb(235, 89, 255)"
+    //     })
+    //     item.appendChild(promoteButton)
+    //     item.appendChild(deleteButton)
+    //     item.appendChild(moveUpButton)
+    //     item.appendChild(moveDownButton)
         //localStorage.setItem('dinoplasty', JSON.stringify(this.dinos));
         this.save()
         return item    
@@ -264,6 +289,78 @@ const app ={
             this.max=dinoCounter
         }
     },
+
+
+    moveUP(ev){
+        const listItem=ev.target.closest('.dino')
+
+        for(let index=0;index<this.dinos.length;index++){
+            const currentId= this.dinos[index].id.toString()
+            if(listItem.dataset.id===currentId){
+                if(index>0){
+                    const dinoBefore=this.dinos[index-1];
+                    const dinoNow=this.dinos[index];
+                    this.dinos[index]=dinoBefore
+                    this.dinos[index-1]=dinoNow
+                    //Debugging: alert(this.dinos)
+                    while(this.list.firstChild){
+                    this.list.removeChild(this.list.firstChild);
+                    }
+                    for (let index = 0; index < this.dinos.length; index++) {
+                    this.list.appendChild(this.renderListItem(this.dinos[index]))
+                    }
+                }
+                
+                break;
+            }
+        }
+       this.save()
+
+    },
+
+    moveDown(ev){
+        const listItem=ev.target.closest('.dino')
+
+        for(let index=0;index<this.dinos.length;index++){
+            const currentId= this.dinos[index].id.toString()
+            if(listItem.dataset.id===currentId){
+
+                if(index<this.dinos.length-1){
+                    const dinoNext=this.dinos[index+1];
+                    const dinoNow=this.dinos[index];
+                    this.dinos[index]=dinoNext
+                    this.dinos[index+1]=dinoNow
+                    //Debugging: alert(this.dinos)
+                    while(this.list.firstChild){
+                    this.list.removeChild(this.list.firstChild);
+                    }
+                    for (let index = 0; index < this.dinos.length; index++) {
+                    this.list.appendChild(this.renderListItem(this.dinos[index]))
+                    }
+                }
+                
+                break;
+            }
+        }
+       this.save()
+
+    },
+
+    favDino(ev){
+        const listItem=ev.target.closest('.dino')
+        listItem.style.backgroundColor="rgb(235, 89, 255)"
+        for(let i=0;i<this.dinos.length;i++){
+            const currentId= this.dinos[i].id.toString()
+            if(listItem.dataset.id===currentId){
+                this.dinos[i].promoted=1
+                
+                break;
+            }
+        }
+       this.save()
+
+    },
+
     removeDino(ev){
         const listItem=ev.target.closest('.dino')
         listItem.remove()
@@ -271,7 +368,12 @@ const app ={
             const currentId= this.dinos[i].id.toString()
             if(listItem.dataset.id===currentId){
                 this.dinos.splice(i,1)
-                
+                while(this.list.firstChild){
+                    this.list.removeChild(this.list.firstChild);
+                    }
+                    for (let index = 0; index < this.dinos.length; index++) {
+                    this.list.appendChild(this.renderListItem(this.dinos[index]))
+                    }
                 break;
             }
         }
